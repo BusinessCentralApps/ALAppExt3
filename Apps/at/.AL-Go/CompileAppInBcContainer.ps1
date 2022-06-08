f6077b13b359722d682c35ed92242ee726985ea1
freddydk <freddyk@microsoft.com>
2022-06-08 21:10:52 +0200

    publish early

--- Apps/at/.AL-Go/CompileAppInBcContainer.ps1
+++ Apps/at/.AL-Go/CompileAppInBcContainer.ps1
@@ -9,6 +9,7 @@ if ($appFile) {
     if ($filename -like "Microsoft_System Application_*.*.*.*.app") {
         # System application compiled - add BaseApp and Application app from container to output
         Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { Param([string]$packagesFolder)
+            New-Item -Path "c:\run\my\testtoolkit" -ItemType container | Out-Null
             $baseApp = "C:\Applications.*\Microsoft_Base Application_*.*.*.*.app"
             $applicationApp = "C:\Applications.*\Microsoft_Application_*.*.*.*.app"
             $testLibrariesApp = "C:\Applications.*\Microsoft_Tests-TestLibraries_*.*.*.*.app"
@@ -24,25 +25,27 @@ if ($appFile) {
             Write-Host "Copying Base Application to packages path"
             Copy-Item -Path $baseApp -Destination (Join-Path $packagesFolder "Microsoft_Base Application.app")
             Copy-Item -Path $baseApp -Destination "c:\run\my\Microsoft_Base Application.app"
+            Copy-Item -Path $baseApp -Destination "c:\run\my\testtoolkit\Microsoft_Base Application.app"
             
             Write-Host "Copying Application to packages path"
             Copy-Item -Path $applicationApp -Destination (Join-Path $packagesFolder "Microsoft_Application.app")
             Copy-Item -Path $applicationApp -Destination "c:\run\my\Microsoft_Application.app"
+            Copy-Item -Path $applicationApp -Destination "c:\run\my\testtoolkit\Microsoft_Application.app"
 
             Write-Host "Copying Tests-TestLibraries to packages path"
             Copy-Item -Path $testLibrariesApp -Destination (Join-Path $packagesFolder "Microsoft_Tests-TestLibraries.app")
             Copy-Item -Path $testLibrariesApp -Destination "c:\run\my\Microsoft_Tests-TestLibraries.app"
-            New-Item -Path "c:\run\my\testtoolkit" -ItemType container | Out-Null
             Copy-Item -Path $testLibrariesApp -Destination "c:\run\my\testtoolkit\Microsoft_Tests-TestLibraries.app"
         } -argumentList (Get-BcContainerPath -ContainerName $parameters.ContainerName -path $Parameters.appSymbolsFolder)
     }
     if ($filename -like "Microsoft_Any.app" -or $filename -like "Microsoft_Any_*.*.*.*.app" -or
+            $filename -like "Microsoft_System Application.app" -or $filename -like "Microsoft_System Application_*.*.*.*.app" -or
             $filename -like "Microsoft_Library Assert.app" -or $filename -like "Microsoft_Library Assert_*.*.*.*.app" -or
             $filename -like "Microsoft_Permissions Mock.app" -or $filename -like "Microsoft_Permissions Mock_*.*.*.*.app" -or
             $filename -like "Microsoft_Library Variable Storage.app" -or $filename -like "Microsoft_Library Variable Storage_*.*.*.*.app" -or
             $filename -like "Microsoft_System Application Test Library.app" -or $filename -like "Microsoft_System Application Test Library_*.*.*.*.app" -or
             $filename -like "Microsoft_Test Runner.app" -or $filename -like "Microsoft_Test Runner_*.*.*.*.app") {
-        Copy-Item -Path $appfile -Destination "c:\run\my\testtoolkit\" -Force
+        Copy-Item -Path $appfile -Destination (Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\testtoolkit") -Force
     }
 }
 
--- Apps/at/.AL-Go/PublishBcContainerApp.ps1
+++ Apps/at/.AL-Go/PublishBcContainerApp.ps1
@@ -3,41 +3,41 @@ Param(
 )
 
 $parameters.appfile | Out-Host
-
-$systemAppFile = $parameters.appfile | Where-Object { [System.IO.Path]::GetFileName($_) -like "Microsoft_System Application_*.*.*.*.app" }
-if ($systemAppFile) {
-    if (!$parameters.ContainsKey('includeOnlyAppIds')) {
-        $parameters.includeOnlyAppIds = @()
-    }
-    if (!$parameters.ContainsKey('SkipVerification')) {
-        $parameters.SkipVerification = $false
-    }
-    $includeOnlyAppIds = $parameters.includeOnlyAppIds
-    $skipVerification = $parameters.SkipVerification
-    $remainingAppFiles = $parameters.appfile | Where-Object { $_ -ne $systemAppFile }
-    $parameters.AppFile = $systemAppFile
-    $parameters.includeOnlyAppIds = @()
-
+#
+#$systemAppFile = $parameters.appfile | Where-Object { [System.IO.Path]::GetFileName($_) -like "Microsoft_System Application_*.*.*.*.app" }
+#if ($systemAppFile) {
+#    if (!$parameters.ContainsKey('includeOnlyAppIds')) {
+#        $parameters.includeOnlyAppIds = @()
+#    }
+#    if (!$parameters.ContainsKey('SkipVerification')) {
+#        $parameters.SkipVerification = $false
+#    }
+#    $includeOnlyAppIds = $parameters.includeOnlyAppIds
+#    $skipVerification = $parameters.SkipVerification
+#    $remainingAppFiles = $parameters.appfile | Where-Object { $_ -ne $systemAppFile }
+#    $parameters.AppFile = $systemAppFile
+#    $parameters.includeOnlyAppIds = @()
+#
+#    Publish-BcContainerApp @parameters
+#
+#    Copy-Item -Path $systemAppFile -Destination (Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my")
+#
+#    Write-Host "Publishing Base Application"
+#    $parameters.SkipVerification = $true
+#    $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Base Application.app"
+#    Publish-BcContainerApp @parameters
+#
+#    Write-Host "Publishing Application"
+#    $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Application.app"
+#    Publish-BcContainerApp @parameters
+#
+#    if ($remainingAppFiles) {
+#        $parameters.AppFile = $remainingAppFiles
+#        $parameters.includeOnlyAppIds = $includeOnlyAppIds
+#        $parameters.SkipVerification = $skipVerification
+#        Publish-BcContainerApp @parameters
+#    }
+#}
+#else {
     Publish-BcContainerApp @parameters
-
-    Copy-Item -Path $systemAppFile -Destination (Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my")
-
-    Write-Host "Publishing Base Application"
-    $parameters.SkipVerification = $true
-    $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Base Application.app"
-    Publish-BcContainerApp @parameters
-
-    Write-Host "Publishing Application"
-    $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Application.app"
-    Publish-BcContainerApp @parameters
-
-    if ($remainingAppFiles) {
-        $parameters.AppFile = $remainingAppFiles
-        $parameters.includeOnlyAppIds = $includeOnlyAppIds
-        $parameters.SkipVerification = $skipVerification
-        Publish-BcContainerApp @parameters
-    }
-}
-else {
-    Publish-BcContainerApp @parameters
-}
+#}
