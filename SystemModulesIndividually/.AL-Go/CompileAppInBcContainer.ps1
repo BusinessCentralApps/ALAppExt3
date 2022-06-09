63b0796c8c59d0f32d623e585dd856e326236798
freddydk <freddyk@microsoft.com>
2022-06-09 12:47:17 +0200

    modify settings

--- SystemModulesIndividually/.AL-Go/CompileAppInBcContainer.ps1
+++ SystemModulesIndividually/.AL-Go/CompileAppInBcContainer.ps1
@@ -9,20 +9,28 @@ if ($appFile) {
     if ($filename -like "Microsoft_System Application_*.*.*.*.app") {
         # System application compiled - add BaseApp and Application app from container to output
         Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { Param([string]$packagesFolder)
-            $baseApp = "C:\Applications.*\Microsoft_Base Application_*.*.*.*.app"
-            $application = "C:\Applications.*\Microsoft_Application_*.*.*.*.app"
-            if (-not (Test-Path $baseApp)) {
-                $baseApp = "C:\Applications\BaseApp\Source\Microsoft_Base Application.app"
+            function CopyApp {
+                Param(
+                    [string] $localAppPath,
+                    [string] $w1AppPath
+                )
+
+                $name = [System.IO.Path]::GetFileName($w1AppPath)
+                Write-Host "Copying $name to packages path"
+                if (Test-Path $localAppPath) {
+                    $appPath = $localAppPath
+                }
+                else {
+                    $appPath = $w1AppPath
+                }
+                Copy-Item -Path $appPath -Destination (Join-Path $packagesFolder $name)
+                Copy-Item -Path $appPath -Destination (Join-Path "c:\run\my" $name)
             }
-            if (-not (Test-Path $application)) {
-                $application = "C:\Applications\Application\Source\Microsoft_Application.app"
-            }
-            Write-Host "Copying Base Application to packages path"
-            Copy-Item -Path $baseApp -Destination (Join-Path $packagesFolder "Microsoft_Base Application.app")
-            
-            Write-Host "Copying Application to packages path"
-            Copy-Item -Path $application -Destination (Join-Path $packagesFolder "Microsoft_Application.app")
+
+            CopyApp -localAppPath "C:\Applications.*\Microsoft_Base Application_*.*.*.*.app"    -w1AppPath "C:\Applications\BaseApp\Source\Microsoft_Base Application.app"
+            CopyApp -localAppPath "C:\Applications.*\Microsoft_Application_*.*.*.*.app"         -w1AppPath "C:\Applications\Application\Source\Microsoft_Application.app" 
         } -argumentList (Get-BcContainerPath -ContainerName $parameters.ContainerName -path $Parameters.appSymbolsFolder)
     }
 }
+
 $appFile
