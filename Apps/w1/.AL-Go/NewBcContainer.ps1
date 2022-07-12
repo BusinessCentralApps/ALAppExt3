37a5f3661bc0f3ebb530f572384f34fcf7f500fb
Ihor Handziuk <ihhandzi@microsoft.com>
2022-07-12 13:39:47 +0200

    put bacl the unpublishing logic

--- Apps/w1/.AL-Go/NewBcContainer.ps1
+++ Apps/w1/.AL-Go/NewBcContainer.ps1
@@ -14,8 +14,9 @@ New-BcContainer @parameters
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
+    $removeData = $false
     Write-Host "Removing $($_.Name)"
-    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -force
+    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData:$removeData -doNotSaveSchema:$removeData -force
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
